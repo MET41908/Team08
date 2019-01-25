8551676cd953ed26b327dbf1565333ec9a2c9702
MET419 <46632788+MET419@users.noreply.github.com>
2019-01-25 15:44:14 -0800

    Set driveForDistancePID to 10 meters
    Set driveForDistancePID to 10 meters

--- src/auto.c
+++ src/auto.c
@@ -36,9 +36,9 @@ void autonomous() {
      case 0:
        drivePID(60);      //will run forever until powered off
      case 1:
-       driveForDistancePID(36, 60);     // for 36" at speed 60
-       pivotTurn(0, 30, 90);            // turn at seed 30 for 90 degree angle right turn
-       driveForDistancePID(12, 50);     // After turn drive a bit more forward
+       driveForDistancePID(394, 60);     // for 36" at speed 60
+       //pivotTurn(0, 30, 90);            // turn at seed 30 for 90 degree angle right turn
+       //driveForDistancePID(12, 50);     // After turn drive a bit more forward
      break;
 
      case 2:
